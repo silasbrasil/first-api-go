081fd29c9b171e3e7adb72680b3e55c06a6d0a34
Silas Brasil <silas.swf@gmail.com>
2025-02-05 17:28:51 -0300

    create models and POST method to save new book

--- controllers/books.go
+++ controllers/books.go
@@ -4,8 +4,32 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/silasbrasil/first-api-go/models"
 )
 
 func ListBooks(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"book": "My tile here"})
+	var books []models.Book
+
+	models.DB.Find(&books)
+
+	c.JSON(http.StatusOK, gin.H{"book": books})
+}
+
+type CreateBookInput struct {
+	Title  string `json:"title" binding:"required"`
+	Author string `json:"author" binding:"required"`
+}
+
+func CreateBook(c *gin.Context) {
+	var input CreateBookInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	book := models.Book{Title: input.Title, Author: input.Title}
+	models.DB.Create(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
 }
--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/silasbrasil/first-api-go/controllers"
+	"github.com/silasbrasil/first-api-go/models"
 )
 
 func main() {
 	r := gin.Default()
 
 	r.GET("/books", controllers.ListBooks)
+	r.POST("/books", controllers.CreateBook)
+
+	models.ConnectDatabase()
 
 	r.Run()
 }
